35dde9fe8ad1dfad655413eecf2be49b2d33ca0b
Skippysunday <skippysunday@gmail.com>
2021-12-05 16:47:52 -0600

    Initial Commit

---
+++ src/main/java/me/skippysunday/playerstats/PlayerStats.java
@@ -0,0 +1,17 @@
+package me.skippysunday.playerstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

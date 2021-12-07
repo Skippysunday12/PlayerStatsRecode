a0a861415fb5f59e0474019807ac3348a25fc43b
Skippysunday <skippysunday@gmail.com>
2021-12-06 18:48:48 -0600

    Finished live updating from an InventoryCreator

--- src/main/java/me/skippysunday/gui/LiveUpdateListener.java
+++ src/main/java/me/skippysunday/gui/LiveUpdateListener.java
@@ -1,15 +1,23 @@
 package me.skippysunday.gui;
 
+import me.skippysunday.playerstats.PlayerStats;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import static me.skippysunday.gui.LiveUpdateRegister.updates;
 
 public class LiveUpdateListener implements Listener {
     @EventHandler
     public void onClose(InventoryCloseEvent e) {
-        if(updates.containsKey(e.getPlayer())) updates.get(e.getPlayer()).cancel();
-        updates.remove(e.getPlayer());
+        if (!updates.containsKey(e.getPlayer())) return;
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (e.getPlayer().getOpenInventory().getTitle().equalsIgnoreCase("crafting")) {
+                    updates.remove(e.getPlayer()).cancel();
+                }}}.runTaskLater(PlayerStats.getPlugin(PlayerStats.class), 2);
     }
 }
